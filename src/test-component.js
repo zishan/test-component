07aa7777f38a083646dae70671c5a634a61c5872
Zishan Ahmad <zahmad@teladoc.com>
2016-08-27 22:09:18 -0400

    first pass

---
+++ src/test-component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class TestComponent extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h1>TEST COMPONENT</h1>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, '.');
+
+module.exports = {
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
+  entry: {
+    app: APP_DIR + '/src/test-component.js',
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'test-component.lib.js',
+    library: 'TestComponent',
+    libraryTarget: 'var',
+  },
+  externals: {
+    'react': 'commonjs react',
+    'react-dom' : 'commonjs react-dom',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css',
+        ],
+      },
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: [
+            'react',
+            'es2015',
+          ],
+        },
+      },
+    ],
+  },
+}
